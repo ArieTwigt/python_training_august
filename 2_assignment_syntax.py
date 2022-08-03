905077d7bf7a162670bd548aba014279436123b0
arietwigt <twigt.arie@gmail.com>
2022-08-03 13:27:19 +0200

    added solutions for assignments chapter 2

---
+++ 2_assignment_syntax.py
@@ -0,0 +1,56 @@
+## Assignments
+
+# Assignment 1.
+
+#%%
+first_name = "Erling"
+last_name  = "Haaland"
+
+# (using + instead of an f-string)
+#%% a.
+full_name = first_name + " " + last_name + " Jr."
+print(full_name)
+
+#%% b. Using an f-string
+full_name = f"{first_name} {last_name} Jr."
+print(full_name)
+# %%
+
+
+## Assignment 2.
+
+#%% a.
+full_name_new = f"{first_name[0]}. {last_name} Jr."
+print(full_name_new)
+
+# %% b. (in case the seperate variables first_name and last_name are not available)
+full_name_list =  full_name.split(" ")
+print(full_name_list)
+full_name_new = f"{full_name_list[0][0]}. {full_name_list[1]} {full_name_list[2]}"
+print(full_name_new)
+
+
+## Assignment 3
+
+# %% Set the variables
+flower_list_1 = ['rose', 'tulip', 'lily']
+flower_list_2 = ['dandelion', 'gerbera']
+
+combined_flower_list = flower_list_1 + flower_list_2
+print(combined_flower_list)
+
+# %% a
+print(combined_flower_list)
+combined_flower_list[1] = "daisy"
+print(combined_flower_list)
+
+# %% b. In situations in which you are not sure in which position "tulip" is
+print(combined_flower_list)
+idx = combined_flower_list.index("tulip")
+combined_flower_list[idx] = "daisy"
+print(combined_flower_list)
+
+# %% c. List comprehension (Best solution) In situations where there are more tulips or there is no tulip at all
+print(combined_flower_list)
+combined_flower_list = ["daisy" if x == "tulip" else x for x in combined_flower_list]
+print(combined_flower_list)
--- 2_exp_variables.py
+++ 2_exp_variables.py
@@ -28,4 +28,55 @@ my_name.upper().replace("A", "U")
 # %%
 help(str)
 
+## List operations
+
+# %%
+
+# %%
+len(numbers_list)
+# %%
+len(my_name)
+# %%
+names_list = ["Arie", "Bart", "Cindy", "Arie", "Bart"]
+
+#%% get the unique names
+list(set(names_list))
+# %%
+names_list[0][0]
+# %%
+'''
+Objects:
+[] : lists
+{} : dictionaries
+() : tuples
+
+Operations:
+[] : Data section, indexing
+{} : String operations
+() : Calling an object, using it in a function
+
+'''
+
+#%%
+f"My name is {my_name}"
+
+# %%
+names_list = ["Arie", "Bart", "Cindy", "Arie", "Bart"]
+
+# %%
+names_tuple = ("Arie", "Bart", "Cindy", "Arie", "Bart")
+
+
+# %%
+names_list[0] = "Jim"
+print(names_list)
+# %%
+names_tuple[0] = "Jim"
+print(names_tuple)
+# %%
+names_list.remove("Arie")
+# %%
+names_list
+# %%
+my_name.upper()
 # %%
