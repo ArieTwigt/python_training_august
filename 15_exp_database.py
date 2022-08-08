4627dabc0d5b3cfbb705211273c6a2d905581385
arietwigt <twigt.arie@gmail.com>
2022-08-08 11:29:21 +0200

    added examples of working with db

---
+++ 15_exp_database.py
@@ -0,0 +1,49 @@
+#%%
+from venv import create
+from sqlalchemy import create_engine
+
+#%%
+engine = create_engine('sqlite:///cars.db')
+
+# %%
+engine 
+
+# %%
+# create a table in the database
+qry = '''
+      CREATE TABLE stocks
+      (date text, 
+      trans text, 
+      symbol text, 
+      qty real, 
+      price real)
+               '''
+
+#%% Insert a row of data
+engine.execute(qry)
+
+# %% Add data to the table
+qry_insert = '''
+            INSERT INTO stocks 
+            VALUES ('2006-01-05','BUY','APPL',80,35.14)
+'''
+
+#%%
+engine.execute(qry_insert)
+
+# %%
+engine.table_names()
+
+# %% fetch data from a database
+qry_select = "select * from stocks"
+
+#%%
+result = engine.execute(qry_select)
+
+#%%
+results = result.fetchall()
+
+# %%
+colnames = result.keys()
+
+# %%
