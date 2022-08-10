9cb609356369dfdc6b09628105b84e66bc76c625
arietwigt <twigt.arie@gmail.com>
2022-08-10 10:01:34 +0200

    added examples for decorators

---
+++ exp_decorators.py
@@ -0,0 +1,31 @@
+# %% define a decorator that will correct for the inflation
+def inflation_adjust(func):
+    def wrapper(amount, rate, years):
+        return func(amount, rate / 2, years)
+    
+    return wrapper
+
+
+#%% define a decorator that prints the intermediate results
+def print_interest(func):
+    def wrapper(amount, rate, years):
+        for year in range(years):
+            amount = func(amount, rate, 1)
+            print(amount)
+        return amount
+    return wrapper
+        
+
+# %%
+@inflation_adjust
+@print_interest
+def calc_interest(amount, rate, years):
+    return amount * (1 + rate) ** years
+
+
+
+#%%
+calc_interest(1000, 0.05, 5)
+
+
+# %%
